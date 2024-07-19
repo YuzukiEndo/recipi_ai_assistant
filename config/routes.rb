85a5d4fcb8383ca3e6091572732581950138caea
YuzukiEndo <yuzu.mame.endo@gmail.com>
2024-07-19 18:02:14 +0900

    ログイン、ログアウト、パスワードのバックエンド部分の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'pages/home'
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
+  get 'users/new'
+  get 'users/create'
+
+  root 'pages#home' #また後で変えるかも
+  resources :users, only: %i[new create]
+
+  # ユーザー登録
+  get 'signup', to: 'users#new'
+
+  # ログイン・ログアウト
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
 end
