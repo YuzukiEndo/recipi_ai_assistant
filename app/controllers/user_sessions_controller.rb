be8006b7c91734bd3b855411d1a462ca844e51c0
YuzukiEndo <yuzu.mame.endo@gmail.com>
2024-07-23 21:03:57 +0900

    ビューの軽い修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      flash[:success] = 'ユーザー認証に成功しました。健康維持のための指示に従ってください'
+      flash[:success] = 'ユーザー認証に成功しました。健康維持のための指示に従ってください。'
       redirect_to root_path
     else
       flash[:danger] = 'ユーザー認証に失敗しました。不適切な入力データです。再試行してください。'
