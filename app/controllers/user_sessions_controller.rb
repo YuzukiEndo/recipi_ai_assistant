796272100aee1ec08dc0f52e35de1f84f51e72fd
YuzukiEndo <yuzu.mame.endo@gmail.com>
2024-08-01 17:12:48 +0900

    フラッシュメッセージの修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -10,7 +10,7 @@ class UserSessionsController < ApplicationController
       flash[:success] = 'ユーザー認証に成功しました。健康維持のための指示に従ってください。'
       redirect_to root_path
     else
-      flash.now[:danger] = '不適切な入力データです。再試行してください。'
+      flash.now[:danger] = '認証失敗。不適切な入力データです。再試行してください。'
       render :new
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
       flash[:success] = '登録が完了しました。UTOPIAへようこそ。健康維持のための指示に従ってください。'
       redirect_to root_path
     else
-      flash.now[:danger] = '不適切な入力データです。再試行してください。'
+      flash.now[:danger] = '登録失敗。不適切な入力データです。再試行してください。'
       render :new
     end
   end
