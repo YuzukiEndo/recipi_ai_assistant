3ddb725a9ce63c655e22659a1e467cfae8c2ea2f
YuzukiEndo <yuzu.mame.endo@gmail.com>
2024-08-08 16:57:42 +0900

    修正

---
+++ config/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './app/javascript/packs/application.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'app/assets/builds'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  resolve: {
+    fallback: {
+      global: require.resolve('global')
+    }
+  }
+};
