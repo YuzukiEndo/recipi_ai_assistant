d6672331ee7b440cf383204569a96ceda607ea96
YuzukiEndo <yuzu.mame.endo@gmail.com>
2024-08-08 12:58:28 +0900

    修正

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'app/assets/builds'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
 };
