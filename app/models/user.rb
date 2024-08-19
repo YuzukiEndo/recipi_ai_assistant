ba869ce5da3e712d1479503c1f45b6513898e874
YuzukiEndo <yuzu.mame.endo@gmail.com>
2024-08-19 18:18:20 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, presence: true, uniqueness: true
-  activate_sorcery_module! :reset_password
+  
   
   validates :reset_password_token, uniqueness: true, allow_nil: true
 
