90182074c555b722c3a07eaf38de067cd99171f0
YuzukiEndo <yuzu.mame.endo@gmail.com>
2024-08-20 08:57:25 +0900

    pwリセットエラー

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = user
-    @url  = edit_password_reset_url(@user.reset_password_token)
+    @url  = edit_password_reset_url(@user.reset_password_token,host: 'utopia-app-20240808-f76c709c579e.herokuapp.com'))
     mail(to: user.email, subject: "パスワードリセット")
   end
 end
