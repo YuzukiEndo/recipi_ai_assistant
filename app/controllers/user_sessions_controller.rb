676b8bf2e29ea43b0372edee8db85f3dcbe4234a
YuzukiEndo <yuzu.mame.endo@gmail.com>
2024-08-20 15:04:25 +0900

    修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      flash[:success] = 'ユーザー認証に成功しました。健康維持のための指示に従ってください。'
+      flash[:success] = '認証に成功しました。健康維持のための指示に従ってください。'
       redirect_to root_path
     else
       flash.now[:danger] = '認証失敗。不適切な入力データです。再試行してください。'
@@ -17,7 +17,7 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    flash[:success] = 'ユーザーセッション終了。再認証までアクセス権限が制限されます。'
+    flash[:success] = 'セッション終了。再認証までアクセス権限が制限されます。'
     redirect_to root_path, status: :see_other
   end
 end
