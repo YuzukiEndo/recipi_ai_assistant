37297249197867a8562ad16fab34f54f16324922
YuzukiEndo <yuzu.mame.endo@gmail.com>
2024-08-20 14:57:35 +0900

    エラーの修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -57,7 +57,7 @@ document.addEventListener('turbolinks:load', function() {
   });
 });
 
-document.addEventListener("turbolinks:load", function() {
+document.addEventListener('turbolinks:load', function() {
   if (typeof LineIt !== 'undefined') {
     LineIt.loadButton();
   }
