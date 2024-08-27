8c48c514063109fa2792be8b0445cf268ff055a9
YuzukiEndo <yuzu.mame.endo@gmail.com>
2024-08-27 16:36:35 +0900

    修正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,12 +8,13 @@ class OauthsController < ApplicationController
   def callback
     provider = auth_params[:provider]
     if @user = login_from(provider)
+      Rails.logger.info "User logged in: #{@user.inspect}"
+      Rails.logger.info "Session: #{session.to_h}"
       flash[:success] = "認証に成功しました。健康維持のための指示に従ってください。"
       redirect_to root_path
     else
       begin
         @user = create_from(provider)
-        #reset_session
         auto_login(@user)
         flash[:success] = '認証に成功しました。健康維持のための指示に従ってください。'
         redirect_to root_path
